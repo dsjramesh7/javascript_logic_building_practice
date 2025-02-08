3a1d682d09ee04bf10cf1febeaffa47610b7c162
dsjramesh7 <rameshdussa07@gmail.com>
2025-02-08 10:51:51 +0530

    practice

---
+++ Interview_Prep/JavaScript/17_question.js
@@ -0,0 +1,52 @@
+// 11
+let idea = "anything";
+console.log(idea);
+
+// 12
+const PIValue = Math.PI.toFixed(7);
+console.log(PIValue);
+
+// 13
+let b = "sadfsadf";
+console.log(b);
+b = 34;
+console.log(b);
+
+// 14
+const ty = null;
+console.log(typeof ty);
+
+// 15
+let qw = "23";
+console.log(qw);
+console.log(typeof qw);
+let wq = 23;
+console.log(qw == wq);
+console.log(qw === wq);
+
+// 16
+let isEnter = true;
+console.log(typeof isEnter);
+
+// 17
+let qq = "rey",
+  ww = 12,
+  tt = false;
+console.log(qq, ww, tt);
+
+// 18
+var ss;
+console.log(ss);
+let gg;
+console.log(gg);
+// const rt; // will through error;
+// console.log(rt)
+
+//19
+let gh = undefined;
+console.log(typeof gh);
+
+// 20
+// const arr = [];
+// arr = [];
+// console.log(arr); //Assignment to constant variable.
