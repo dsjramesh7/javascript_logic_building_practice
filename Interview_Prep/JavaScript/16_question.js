e89da5d551849c2d9804a905064e5e7824c20906
dsjramesh7 <rameshdussa07@gmail.com>
2025-02-08 10:36:42 +0530

    practice

--- Interview_Prep/JavaScript/16_question.js
+++ Interview_Prep/JavaScript/16_question.js
@@ -14,3 +14,33 @@ console.log(date.getFullYear());
 const firstName = "kira";
 const lastName = "izuku";
 console.log(`${firstName} ${lastName}`);
+
+// 5
+let a = 12;
+console.log(a);
+a = 69;
+console.log(a);
+
+//6
+console.error("Something went wrong");
+
+// 7
+console.log(Math.pow(12, 2));
+console.log(12 * 12);
+
+// 8
+const isLogged = false;
+console.log(typeof isLogged);
+
+// 9
+const age = 21;
+if (age > 18) {
+  console.log(`${age} is greater than 18`);
+} else if (age === 18) {
+  console.log(`${age} is equal to 18`);
+} else {
+  console.log(`${age} is lesser than 18`);
+}
+
+// 10
+console.log(100 / 0);
