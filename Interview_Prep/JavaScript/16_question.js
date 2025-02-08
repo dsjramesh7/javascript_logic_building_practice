335b99df143c6061798c6a01c79d7e578e9880ec
dsjramesh7 <rameshdussa07@gmail.com>
2025-02-08 10:16:16 +0530

    practice

---
+++ Interview_Prep/JavaScript/16_question.js
@@ -0,0 +1,16 @@
+// 1
+const userName = "kira";
+const hobby = "write";
+console.log(`${userName} likes to ${hobby}`);
+
+// 2
+console.log(45 * 2 - 10);
+
+// 3
+const date = new Date();
+console.log(date.getFullYear());
+
+// 4
+const firstName = "kira";
+const lastName = "izuku";
+console.log(`${firstName} ${lastName}`);
