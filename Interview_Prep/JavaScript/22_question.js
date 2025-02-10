55e7f7d0cd45819f04f674489e2815e04a7004d6
dsjramesh7 <rameshdussa07@gmail.com>
2025-02-11 00:51:05 +0530

    practice

---
+++ Interview_Prep/JavaScript/22_question.js
@@ -0,0 +1,16 @@
+// longest word
+function longestWord(sentence) {
+  let word = sentence.split(" ");
+  // console.log(word);
+  let longestWord = "";
+
+  for (let i = 0; i < word.length; i++) {
+    if (word[i].length > longestWord.length) {
+      longestWord = word[i];
+    }
+  }
+  return longestWord;
+}
+console.log(
+  longestWord("I there what up  asdfasdfsadf 123j12lk3j1l2k3jl12k3 12 555")
+);
