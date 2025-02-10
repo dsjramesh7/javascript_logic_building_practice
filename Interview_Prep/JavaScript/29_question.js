8543db5fa9e655baa8def322a2cd2ee526f299b7
dsjramesh7 <rameshdussa07@gmail.com>
2025-02-11 01:29:44 +0530

    practice

--- Interview_Prep/JavaScript/29_question.js
+++ Interview_Prep/JavaScript/29_question.js
@@ -1,7 +1,10 @@
 // factorial
 function factorial(num) {
-  let fact = num;
-  for (let i = num - 1; i > 0; i--) {
+  let fact = 1;
+  if (num === 0) {
+    return 1;
+  }
+  for (let i = num; i > 0; i--) {
     fact *= i;
   }
   return fact;
