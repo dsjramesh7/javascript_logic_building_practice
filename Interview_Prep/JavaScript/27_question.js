3eabf717cf62693e724609824648bf8b2a2ea922
dsjramesh7 <rameshdussa07@gmail.com>
2025-02-11 01:21:42 +0530

    practice

--- Interview_Prep/JavaScript/27_question.js
+++ Interview_Prep/JavaScript/27_question.js
@@ -1,8 +1,8 @@
 // largest number in an array
 const arrOfNum = [12, 5, 34, 69, 12, 1, 23, 55];
 function largestNumber(arr) {
-  let largestNumber = 0;
-  for (let i = 0; i < arr.length; i++) {
+  let largestNumber = arr[0];
+  for (let i = 1; i < arr.length; i++) {
     if (arr[i] > largestNumber) {
       largestNumber = arr[i];
     }
